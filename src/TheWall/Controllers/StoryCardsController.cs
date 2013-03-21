95a48a6dfd0f852c4266e5817347cb45e77b0e1f
Will Falconer <will@scrumtech.com.au>
2013-03-22 10:21:57 +1100

    Fix up file code style

--- src/TheWall/Controllers/StoryCardsController.cs
+++ src/TheWall/Controllers/StoryCardsController.cs
@@ -1,17 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Web.Http;
-using TheWall.Hubs;
-using TheWall.Models;
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="StoryCardsController.cs" company="nib healthfunds ltd.">
+//   Copyright 2013
+// </copyright>
+// <summary>
+//   Defines the StoryCardsController type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
 
 namespace TheWall.Controllers
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Web.Http;
+
+    using TheWall.Hubs;
+    using TheWall.Models;
+
+    /// <summary>
+    /// The story cards controller.
+    /// </summary>
     public class StoryCardsController : ApiController
     {
         // GET api/storycards
+
+        /// <summary>
+        /// The get. 
+        /// </summary>
+        /// <returns>
+        /// The <see cref="IEnumerable"/>.
+        /// </returns>
         public IEnumerable<StoryCard> Get()
         {
             using (var ravenSession = RavenDataStore.Instance.OpenSession())
@@ -23,6 +40,12 @@ namespace TheWall.Controllers
         }
 
         // GET api/storycards/5
+
+        /// <summary>
+        /// Get by Id
+        /// </summary>
+        /// <param name="id">The Story card id to retrieve.</param>
+        /// <returns>A <see cref="StoryCard"/></returns>
         public StoryCard Get(int id)
         {
             using (var ravenSession = RavenDataStore.Instance.OpenSession())
@@ -32,6 +55,13 @@ namespace TheWall.Controllers
         }
 
         // POST api/storycards
+
+        /// <summary>
+        /// The post.
+        /// </summary>
+        /// <param name="value">
+        /// The value.
+        /// </param>
         public void Post([FromBody]StoryCard value)
         {
             using (var ravenSession = RavenDataStore.Instance.OpenSession())
@@ -45,6 +75,16 @@ namespace TheWall.Controllers
         }
 
         // PUT api/storycards/5
+
+        /// <summary>
+        /// The put.
+        /// </summary>
+        /// <param name="id">
+        /// The id.
+        /// </param>
+        /// <param name="value">
+        /// The value.
+        /// </param>
         public void Put(int id, [FromBody]StoryCard value)
         {
             value.Id = id;
@@ -59,6 +99,16 @@ namespace TheWall.Controllers
         }
 
         // DELETE api/storycards/5
+
+        /// <summary>
+        /// The delete.
+        /// </summary>
+        /// <param name="id">
+        /// The id.
+        /// </param>
+        /// <exception cref="NotImplementedException">
+        /// Not yet implemented.
+        /// </exception>
         public void Delete(int id)
         {
             throw new NotImplementedException();
