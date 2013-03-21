a96fde5970e79aa823890ae38423e13ce5b26c9d
Will Falconer <will@scrumtech.com.au>
2013-03-21 14:25:20 +1100

    Remove build related files and add signalR test client.

---
+++ src/TheWall/Hubs/ChatHub.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TheWall.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public void Send(string name, string message)
+        {
+            // call broadcast message
+            Clients.All.broadcastMessage(name, message);
+        }
+    }
+}
