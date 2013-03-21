f29cced54eb6c9b4181acef219d9c59102cdfcd8
Will Falconer <will@scrumtech.com.au>
2013-03-21 14:26:38 +1100

    SignalR stuff

--- src/TheWall/Controllers/WallController.cs
+++ src/TheWall/Controllers/WallController.cs
@@ -15,19 +15,6 @@
 
         public ActionResult Index()
         {
-            // create a story card
-            this.RavenSession.Store(
-                new StoryCard()
-                    {
-                        Id = 1,
-                        AcceptanceCriteria = "This is a story card",
-                        StoryPoints = 5,
-                        Title = "As a user I want to do something so that I can achieve a goal."
-                    });
-
-            // load a story card
-            var storyCard = this.RavenSession.Load<StoryCard>(1);
-
             return View("Index");
         }
 
--- src/TheWall/Global.asax.cs
+++ src/TheWall/Global.asax.cs
@@ -4,6 +4,7 @@
     using System.Web.Mvc;
     using System.Web.Optimization;
     using System.Web.Routing;
+    using Microsoft.AspNet.SignalR;
 
     // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
     // visit http://go.microsoft.com/?LinkId=9394801
@@ -14,6 +15,7 @@
         {
             AreaRegistration.RegisterAllAreas();
 
+            RouteTable.Routes.MapHubs();
             WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
@@ -21,7 +23,6 @@
             AuthConfig.RegisterAuth();
 
             RavenDataStore.Initialize();
-            RouteTable.Routes.MapHubs();
         }
     }
 }
